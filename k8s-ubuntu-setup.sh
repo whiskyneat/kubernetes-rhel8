c9ef678bba33905987c176d5f90b0f8c68584c81
Whisky Neat <whiskyneat@example.com>
2022-02-15 20:55:50 -0700

    Added ubuntu script v1

---
+++ k8s-ubuntu-setup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#1 Master, 3 Nodes
+#
+
+#### 1.0 Basics ####
+
+#disable swap
+sudo swapoff -a
+
+#Install Packages
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+#setup required sysctl params,
+sudo cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+#Apply sysctl params without reboot
+sudo sysctl --system
+
+#### 2.0 Install containerd ####
+sudo apt-get update 
+sudo apt-get install -y containerd
+
+#Create a containerd configuration file
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+#Change cgroup driver to containerd
+sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+#Restart containerd with the new configuration
+sudo systemctl restart containerd
+
+
+
+#Install Kubernetes packages - kubeadm, kubelet and kubectl
+#Add Google's apt repository gpg key
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+#Add the Kubernetes apt repository
+sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF'
+
+#Update the package list and use apt-cache policy to inspect versions available in the repository
+sudo apt-get update
+sudo apt-cache policy kubelet | head -n 20
+
+#Install the required packages, if needed we can request a specific version. 
+#Use this version because in a later course we will upgrade the cluster to a newer version.
+VERSION=1.22.4-00
+sudo apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION
+sudo apt-mark hold kubelet kubeadm kubectl containerd
+
+#Check the status of our kubelet and our container runtime, containerd.
+#The kubelet will enter a crashloop until a cluster is created or the node is joined to an existing cluster.
+sudo systemctl status kubelet.service 
+sudo systemctl status containerd.service 
+
+
+#Ensure both are set to start when the system starts up.
+sudo systemctl enable kubelet.service
+sudo systemctl enable containerd.service
